cce8f8f069fe5167caf928fe3ff33fd667213332
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-08-16 12:56:03 +0530

    Final

--- Vendingmachine.cpp
+++ Vendingmachine.cpp
@@ -1,50 +1,53 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
-bool On=true;
-
+ 
+bool On = true;
 void showOptions() {
     cout << "\nAvailable Options:\n";
     cout << "1. Espresso\n";
     cout << "2. Latte\n";
     cout << "3. Mocha\n";
 }
+ 
 void selectProcess() {
-    int choice;
+    string input;
  
     while (true) {
         cout << "Enter your selection: ";
-        cin >> choice;
+        cin >> input;
  
+        if (input.length() != 1 || (input[0] < '1' || input[0] > '3')) {
+            cout << "Invalid selection! Choose 1, 2, or 3.\n";
+            showOptions();
+            continue;
+        }
+ 
+        char choice = input[0];
         switch(choice) {
-            case 1: // Espresso
-                cout << "Preparing Hot coffee...\n";
-                cout << "Hot coffee\n";
-            case 2: // Latte
-                if(choice >= 2) {
-                cout << "Preparing Latte...\n";
-                cout << "Adding frothed milk\n";
-                }
-            case 3: // Mocha
-                if(choice == 3)
-                cout << "Preparing Mocha...\n"; 
-                cout << "Adding chocolate powder\n";
-                return; 
-            default:
-                if(choice < 1 || choice > 3) {
-                    cout << "Invalid selection!\n";
-                    showOptions();
-                }
-                break;
+            case '3': cout << "Preparing Mocha...\nAdding chocolate powder\n";
+            case '2': cout << "Adding frothed milk\n";
+            case '1': cout << "Hot coffee\n"; return;
         }
     }
 }
-int main(){
-     cout<< "Vending machine\n";
-     cout<< "Powering on...\n";
-     while (On){
-        selectProcess();
-     }
-     return 0;
+ 
+void terminationProcess() {
+    string input;
+    cout << "\nPress 'P' to power off or any other key to continue: ";
+    cin >> input;
+    if(!input.empty() && (input[0]=='P'||input[0]=='p')) On = false;
 }
-
+ 
+int main() {
+    cout << "Vending Machine Simulation Started\nPowering On...\n";
+ 
+    while (On) {
+        showOptions();
+        selectProcess();
+        terminationProcess();
+    }
+ 
+    cout << "Vending Machine Powered Off.\n";
+    return 0;
+}
