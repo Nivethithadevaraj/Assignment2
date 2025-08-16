4a9047a0a20973a986f66e7cedec697294bd0cf5
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-08-16 10:47:05 +0530

    second change

--- Vendingmachine.cpp
+++ Vendingmachine.cpp
@@ -2,30 +2,42 @@
 using namespace std;
 
 bool On=true;
+
+void selectProcess() {
+    int choice;
+ 
+    while (true) {
+        cout << "Enter your selection: ";
+        cin >> choice;
+ 
+        switch(choice) {
+            case 1: // Espresso
+                cout << "Preparing Hot coffee...\n";
+                cout << "Hot coffee\n";
+            case 2: // Latte
+                if(choice >= 2) {
+                cout << "Preparing Latte...\n";
+                cout << "Adding frothed milk\n";
+                }
+            case 3: // Mocha
+                if(choice == 3)
+                cout << "Preparing Mocha...\n"; 
+                cout << "Adding chocolate powder\n";
+                return; 
+            default:
+                if(choice < 1 || choice > 3) {
+                    cout << "Invalid selection!\n";
+                }
+                break;
+        }
+    }
+}
 int main(){
      cout<< "Vending machine\n";
      cout<< "Powering on...\n";
      while (On){
-        Selectprocess();
+        selectProcess();
      }
      return 0;
 }
 
-void Selectprocess(){
-    int choice;
-    cout<<"Enter your selection";
-    cin>> choice;
-
-    switch (choice)
-    {
-    case 1:
-        cout<< "Preparing Mocha\n";
-        cout<< "Adding chocolate powder\n";
-    case 2:
-        cout<< "preparing latte\n";
-        cout<<"dding frothed milk\n";
-    case 3:
-        cout<<"preparing espresso\n";
-        cout<<" adding hot coffee\n";
-    }
-}
