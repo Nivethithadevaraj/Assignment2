f09fc8068986b0d60318494523624d866fe5759a
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-08-18 06:52:31 +0530

    second

--- login.cpp
+++ login.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+bool checkAttempts(int attempts) {
+    if (attempts >= 5) {
+        cout << "Account locked due to 5 unsuccessful attempts.\n";
+        cout << "Alert: Please contact support.\n";
+        return true;
+    }
+    return false;
+}
+
 int main() {
     int attempts = 0;
     bool accessGranted = false;
