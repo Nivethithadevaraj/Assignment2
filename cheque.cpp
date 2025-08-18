08484f78e1eaba80f1fd16869a9df6382db1c07d
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-08-18 09:19:39 +0530

    first

---
+++ cheque.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Banking Process Start\n";
+    cout << "Initialization process:\n";
+    initializationProcess();
+    cout << "Review process:\n";
+    reviewProcess();
+    cout << "End of Banking Transaction.\n";
+    return 0;
+}
+ 
