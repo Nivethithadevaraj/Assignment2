eb20f825927460478774a2ebabd7fb75f9c89d1b
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-08-18 09:20:47 +0530

    third

--- cheque.cpp
+++ cheque.cpp
@@ -14,6 +14,40 @@ char getYesNo(const string& prompt) {
         cin.ignore(10000, '\n');
     }
 }
+void receiverDetails() {
+    string receiverAccount;
+    cout << "Enter receiver bank account: ";
+    cin >> receiverAccount;
+    cout << "Insert cheque...\n";
+    string value;
+    cout << "Enter cheque value: ";
+    cin >> value;
+    string senderAccount;
+    cout << "Enter sender account: ";
+    cin >> senderAccount;
+    cout << "Receiver details recorded. Return.\n";
+}
+ 
+// Review process (RP)
+void reviewProcess() {
+    char sevenDays = getYesNo("Has it been 7 days since process started? (Y/N): ");
+    if (sevenDays == 'y' || sevenDays == 'Y') {
+        cout << "Money transferred to the reviewer. Return.\n";
+        return;
+    }
+    char senderReview = getYesNo("Does the sender want to review? (Y/N): ");
+    if (senderReview == 'n' || senderReview == 'N') {
+        cout << "Invalid transaction. Return.\n";
+        return;
+    }
+    char senderAuthorize = getYesNo("Does the sender authorize transaction? (Y/N): ");
+    if (senderAuthorize == 'y' || senderAuthorize == 'Y') {
+        cout << "Money transferred to the reviewer. Return.\n";
+    } else {
+        cout << "Invalid transaction. Return.\n";
+    }
+}
+
 int main() {
     cout << "Banking Process Start\n";
     cout << "Initialization process:\n";
