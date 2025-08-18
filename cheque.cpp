295f8c5122c23759a4ffe6a181a1a5a9fea3adc9
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-08-18 09:20:05 +0530

    second

--- cheque.cpp
+++ cheque.cpp
@@ -2,6 +2,18 @@
 #include <string>
 using namespace std;
 
+char getYesNo(const string& prompt) {
+    char answer;
+    while (true) {
+        cout << prompt;
+        cin >> answer;
+        if (answer == 'y' || answer == 'Y' || answer == 'n' || answer == 'N')
+            return answer;
+        cout << "Invalid input. Please enter Y/y or N/n only.\n";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+}
 int main() {
     cout << "Banking Process Start\n";
     cout << "Initialization process:\n";
